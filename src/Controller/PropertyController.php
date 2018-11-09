6963010fba6adade5373b63206e3146299435d75
nico34638 <nico24093@gmail.com>
2018-11-09 21:44:21 +0100

    feature: add search

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -9,6 +9,8 @@ use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 
 class PropertyController extends AbstractController
 {
@@ -29,14 +31,19 @@ class PropertyController extends AbstractController
      */
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
         $properties = $paginator->paginate(
-            $this->repository->findAllVisibleQuery(),
+            $this->repository->findAllVisibleQuery($search),
             $request->query->getInt('page', 1),
             12
         );
         return $this->render('property/index.html.twig', [
-          'current_menu' => 'properties',
-          'properties' => $properties
+          'current_menu'  => 'properties',
+          'properties'    => $properties,
+          'form'          => $form->createView()
         ]);
     }
 
@@ -53,8 +60,8 @@ class PropertyController extends AbstractController
           ], 301);
         }
         return $this->render('property/show.html.twig', [
-          'property' => $property,
-          'current_menu' => 'properties'
+          'property'      => $property,
+          'current_menu'  => 'properties'
         ]);
     }
 }
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -7,6 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Query;
+use App\Entity\PropertySearch;
 
 /**
  * @method Property|null find($id, $lockMode = null, $lockVersion = null)
@@ -24,10 +25,22 @@ class PropertyRepository extends ServiceEntityRepository
     /**
      *@return Query
      */
-    public function findAllVisibleQuery(): Query
+    public function findAllVisibleQuery(PropertySearch $search): Query
     {
-        return $this->findVisibleQuery()
-          ->getQuery();
+        $query = $this->findVisibleQuery();
+
+        if($search->getMaxPrice()){
+          $query = $query
+            ->andWhere('p.price <= :maxprice')
+            ->setParameter('maxprice', $search->getMaxPrice());
+        }
+
+        if($search->getMinSurface()){
+          $query = $query
+            ->andWhere('p.surface >= :minsurface')
+            ->setParameter('minsurface', $search->getMinSurface());
+        }
+        return $query->getQuery();
     }
 
     /**
